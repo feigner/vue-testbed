c4f4303ac8d13ed28de311489fa5a7f1069c8013
eric feigner <feigner@gmail.com>
2018-11-19 17:09:31 -0800

    add curl to req libs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y build-essential gcc g++ make
+sudo apt-get install -y build-essential gcc g++ make curl
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install --global @vue/cli
 sudo npm install --global @vue/cli-init
 
 # npm update check perms
-sudo chown -R $USER:$(id -gn $USER) /home/ubuntu/.config
+# sudo chown -R $USER:$(id -gn $USER) /home/ubuntu/.config
